fbb5b8fb6ffb5785e3960614397375855246b9d2
Andrew Hyland <119887032+AndrewHyland757@users.noreply.github.com>
2023-11-06 08:18:21 +0000

    Add urls.py file, templates folder and static folder to the app. Add functions in the views.py to render the index and register pages.

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+    
+def index(request):
+    return render(request, 'bookings/index.html')
+
+def register(request):
+    return render(request, 'bookings/index.html')
