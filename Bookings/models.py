10f5a217bd4502d6055c47297a23100402008c2c
AndrewHyland757 <hyland757@gmail.com>
2023-11-23 21:07:03 +0000

    Add app urls.py file

--- Bookings/models.py
+++ Bookings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField()
