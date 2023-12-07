3a9b60cc175aba88ffc222a8283bfc815f8cc386
AndrewHyland757 <hyland757@gmail.com>
2023-12-07 22:50:30 +0000

    Add javascript to add brand colour depending on page displayed

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,63 @@
 
 
 
-  function remove(el) {
-    var element = el;
-    element.remove();
-  }
-  
+const navbarBrand = document.querySelector(".navbar-brand")
 
-  
 
-  document.querySelectorAll(".nav-link").forEach((link) => {
-    if (link.href === window.location.href) {
+
+
+
+
+
+if ( window.location.href === 'https://8000-andrewhylan-oishirestau-lo9lr6ujhg0.ws-eu106.gitpod.io/') {
+    navbarBrand.classList.add("brand-home");}
+else {
+    navbarBrand.classList.add("brand");}
+
+
+
+
+document.querySelectorAll(".nav-link").forEach((link) => {
+    if ( window.location.href === 'https://8000-andrewhylan-oishirestau-lo9lr6ujhg0.ws-eu106.gitpod.io/') {
+        link.classList.add("nav-link-color-home");}
+    else {
+        link.classList.add("nav-link-color");}
+    } 
+);
+
+
+
+/* adds the active class to the navigation link of current displayed page */
+document.querySelectorAll(".nav-link").forEach((link) => {
+    if (link.href === "window.location.href") {
         link.parentElement.classList.add("active");
         link.setAttribute("aria-current", "page");
     }
 });
 
 
+
+
+
+
+/* function to remove an element, used on burger icon in nav-bar */
+function remove(element) {
+var element = element;
+element.remove();
+}
+
+
+
+
+/* adds the active class to the navigation link of current displayed page */
 document.querySelectorAll(".nav-link").forEach((link) => {
-    if (window.location.href === "home") {
-        link.classList.add("nav-link-color-home");}
-    else {
-        link.classList.add("nav-link-color");}
+    if (link.href === "window.location.href") {
+        link.parentElement.classList.add("active");
+        link.setAttribute("aria-current", "page");
     }
-    
-);
+});
+
+
 
 
 
